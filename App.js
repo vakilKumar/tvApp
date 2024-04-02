28c5afdce3210b29d3d6535a5d1b6ef766fb5365
vakilKumar <vakilraj26031997@gmail.com>
2024-04-02 16:27:55 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import {View, Text, TouchableOpacity, StyleSheet, Platform} from 'react-native';
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello from {'\n'}React Native {Platform.OS === 'web' ? 'web' : 'android'}!</Text>
+      <TouchableOpacity
+        onPress={() => setCount(count + 1)}
+        style={styles.button}>
+        <Text>Click me ram!</Text>
+      </TouchableOpacity>
+
+      <Text>You clicked {count} times!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#C3E8BD',
+    paddingTop: 40,
+    paddingHorizontal: 10,
+  },
+  button: {
+    backgroundColor: '#ADBDFF',
+    padding: 5,
+    marginVertical: 20,
+    alignSelf: 'flex-start',
+  },
+  title: {
+    fontSize: 40,
+  },
+});
+
+export default App;
